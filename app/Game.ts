f28a534e78f78f4dcea247ac2c166d99d462d033
nileshsride <20863980+nileshdattatraykedare@users.noreply.github.com>
2018-04-03 23:33:29 +0530

    nodemon json

--- app/Game.ts
+++ app/Game.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import * as process from 'process';
 import { createInterface, ReadLineOptions } from 'readline';
-import Controller from './Command';
+import Commands from './Command';
 import TableSurface from './Table';
 const table = new TableSurface();
-const controller = new Controller(table);
+const controller = new Commands(table);
 
 const crlOptions: ReadLineOptions = {
   input: process.stdin,
@@ -12,7 +12,7 @@ const crlOptions: ReadLineOptions = {
   terminal: false,
 };
 
-process.stdout.write('Valid commands: PLACE, LEFT, RIGHT, MOVE, REPORT.\n');
+process.stdout.write('Input commands are: PLACE, LEFT, RIGHT, MOVE, REPORT.\n');
 
 const crl = createInterface(crlOptions);
 
--- app/Position.ts
+++ app/Position.ts
@@ -1,4 +1,3 @@
-
 export interface IPosition {
     x: number;
     y: number;
